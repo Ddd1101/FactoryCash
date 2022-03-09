598783d5f839b0eca5e7f657c95379c275a0319d
raynorhou <raynorhou@tencent.com>
2022-03-09 14:36:35 +0800

    添加当前日期显示

--- View/index.js
+++ View/index.js
@@ -27,6 +27,44 @@ function CalculateSum(){
     }
 
     document.forms["products"]["sumPrice"].value = _sum;
+    var thisTime = getCurrentTime();
+    
+    document.getElementById("thisTime").innerHTML = thisTime;
     
     return false;
 }
+
+function ReLoadPage(){
+    window.location.replace("http://ddd1101.github.io/FactoryCash/View/")
+}
+
+/**
+ * 获取当前时间 格式：yyyy-MM-dd HH:MM:SS
+ */
+ function getCurrentTime() {
+    var date = new Date();//当前时间
+    var month = zeroFill(date.getMonth() + 1);//月
+    var day = zeroFill(date.getDate());//日
+    var hour = zeroFill(date.getHours());//时
+    var minute = zeroFill(date.getMinutes());//分
+    var second = zeroFill(date.getSeconds());//秒
+    
+    //当前时间
+    var curTime = date.getFullYear() + "-" + month + "-" + day
+            + " " + hour + ":" + minute + ":" + second;
+    
+    return curTime;
+}
+ 
+/**
+ * 补零
+ */
+function zeroFill(i){
+    if (i >= 0 && i <= 9) {
+        return "0" + i;
+    } else {
+        return i;
+    }
+}
+
+
